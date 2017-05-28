8ca461aa1699e4572b129125aae5da0f4a06a423
ampleeve <e@ampleev.com>
2017-05-28 13:21:59 +0300

    dsfsdf

--- yii2.com/basic/web/index.php
+++ yii2.com/basic/web/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="google-site-verification" content="XhdXdzYd2S1BGjQod5IxAQ_fBntQFXm1YET37V1fqtQ" />
+    <meta name="google-site-verification" content="M1rpcsgkhAfhoCMcZmsGfzrGbGwQYa_Uz2drqw-M5_Q" />
     <title>Title</title>
 </head>
 <body>
