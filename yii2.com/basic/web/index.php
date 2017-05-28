4abe2a3c9e092694a99c1bc8f1c7e424d92579da
ampleeve <e@ampleev.com>
2017-05-28 15:05:08 +0300

    favicon section

--- yii2.com/basic/web/index.php
+++ yii2.com/basic/web/index.php
@@ -2,6 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    
     <meta name="google-site-verification" content="M1rpcsgkhAfhoCMcZmsGfzrGbGwQYa_Uz2drqw-M5_Q" />
     <title>Title</title>
 </head>
