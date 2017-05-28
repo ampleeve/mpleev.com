78ac0291c2f073f19c1587026c3647b06a03297d
ampleeve <e@ampleev.com>
2017-05-28 15:13:38 +0300

    sf

--- yii2.com/basic/web/index.php
+++ yii2.com/basic/web/index.php
@@ -9,9 +9,9 @@
     <link rel="manifest" href="/manifest.json">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="theme-color" content="#ffffff">
-    
+
     <meta name="google-site-verification" content="M1rpcsgkhAfhoCMcZmsGfzrGbGwQYa_Uz2drqw-M5_Q" />
-    <title>Title</title>
+    <title>e@mpleev.com</title>
 </head>
 <body>
 
